b0f46dd51afad3974b9ccd2efa3775e1be435aa8
Abdallah Galib <abdallah.galib.wasi@gmail.com>
2021-11-26 23:25:23 +0600

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+
+<div>
+
+	<ul>
+		{#each funds as fund}
+			<li on:click = {() => {select(fund)}} class:selected = {fund == selectedFund} grid-2 class = "fund" full><span>{fund.name}</span><span amount>{fund.amount} {fund.cur || 'tk'}</span></li>
+		{/each}
+	</ul>
+	<Editor bind:item = {selectedFund}></Editor>	
+</div>
+
+
+
+<script>
+	import Editor from './editor.svelte'
+	let selectedFund = {}, funds = [
+		{name:'Abdallah', amount:100}, 
+		{name:'Afia Anwar', amount: 3000}, 
+		{name:'Pahely', amount:500}, 
+		{name:'House Hold', amount:120, cur:'$'},
+	]
+	function select(fund){
+		selectedFund = fund
+	}
+</script>
+		 
+<style>
+	[grid-2] {display: grid; grid-template-columns: repeat(2, 1fr);}
+	
+	.fund { gap:2rem; }
+	[amount] {text-align:right;}
+	[full] {width:100%;}
+	ul, li {padding:0.5rem;; margin: 0}
+	* { box-sizing: border-box; }
+	.selected {background: darkgreen; color: whitesmoke;}
+</style>
+
+
+
---
+++ src/editor.svelte
@@ -0,0 +1,8 @@
+<span> 
+    <input bind:value = {item.name} placeholder = "Name" />
+    <input bind:value = {item.amount} placeholder = "Amount" />
+</span>
+
+<script>
+    export let item = {name:'', amount:1}
+</script>
