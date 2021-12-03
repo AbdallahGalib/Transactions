8f118f31b1213dc48173204026e031a7ab9e64c1
Abdallah Galib <abdallah.galib.wasi@gmail.com>
2021-12-03 15:04:52 +0600

    commit lol

--- src/editor.svelte
+++ src/editor.svelte
@@ -1,7 +1,6 @@
 <span> 
     <input bind:value = {item.name} placeholder = "Name" />
     <input bind:value = {item.amount} placeholder = "Amount" />
-    Sorry It isn't Editable Yet Sadly :( We will fix It ASAP. So please Be patient Thank you.
 </span>
 
 <script>
